ce3c70d6444ee4a8bb58d1e43adfec26277f13fe
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-11-25 18:44:45 +0530

    Added and Styled the Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-
+import Header from "./components/Header"
 
 
 function App() {
-  
-
   return (
-    <h1>App</h1>
+    <>
+      <Header />
+    </>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+
+
+function Header() {
+  return (
+    <header className="header">
+      <img src="src/assets/globe.png" alt="globe icon" />
+      <h1>my travel journal.</h1>
+    </header>
+  )
+}
+
+export default Header
